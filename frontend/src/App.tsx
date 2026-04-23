1f8122fc838e655e83354ef480734f2ff55a114d
Nate Schaefer <nathanschaefer14@gmail.com>
2026-04-22 22:48:53 -0500

    added protected and public routes for when user is logged in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,17 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute'
+import PublicRoute from './components/PublicRoute'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<PublicRoute><Home /> </PublicRoute>} />
+        <Route path="/login" element={<PublicRoute><Login /> </PublicRoute>} />
+        <Route path="/register" element={<PublicRoute><Register /> </PublicRoute>} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /> </ProtectedRoute>} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/PublicRoute.tsx
@@ -0,0 +1,15 @@
+import { Navigate } from 'react-router-dom';
+
+interface Props {
+    children: React.ReactNode
+}
+
+export default function PublicRoute({ children }: Props) {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+        return <Navigate to ="/dashboard" />;
+    }
+
+    return <>{children}</>;
+}
